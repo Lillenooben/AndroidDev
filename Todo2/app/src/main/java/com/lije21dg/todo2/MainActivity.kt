0bb93c00c2e52a5b2df0ded286927d0bdb3b9059
Lillenooben <jesperli99@hotmail.com>
2023-01-24 12:52:58 +0100

    Almost finished
    Bottom bar functionality is now correctly implemented.
    Delete will create a new list with new taskIds in order to account for the new list size.
    Editing tasks now work.

--- Todo2/app/src/main/java/com/lije21dg/todo2/MainActivity.kt
+++ Todo2/app/src/main/java/com/lije21dg/todo2/MainActivity.kt
@@ -3,24 +3,35 @@ package com.lije21dg.todo2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.lije21dg.todo2.ui.theme.Todo2Theme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,12 +51,11 @@ class MainActivity : ComponentActivity() {
 }
 
 data class TaskItem(
-    val taskTitle: String,
-    val taskText: String,
-    val taskStatusBool: Boolean
+    var taskTitle: String,
+    var taskId: Int
 )
 
-val testTaskList: MutableList<TaskItem> = mutableListOf()
+var testTaskList: MutableList<TaskItem> = mutableListOf()
 
 @Composable
 fun MainScreen() {
@@ -53,57 +63,74 @@ fun MainScreen() {
     val navController = rememberNavController()
     Column(
         modifier = Modifier
+            .fillMaxSize()
             .background(Color.Red)
             .padding(8.dp)
     ){
+
         NavHost(
             navController,
-            "taskScreen"){
+            "taskScreen",
+        modifier = Modifier.weight(1f)){
             composable("taskScreen"){
-                TaskScreen(testTaskList)
+                TaskScreen(navController)
             }
             composable("createScreen")
             {
                 CreateScreen()
             }
-        }
-        Row(
-            horizontalArrangement = Arrangement.SpaceAround,
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.Yellow)
-        ){
-
-            Button(onClick = {
-                testTaskList.add(TaskItem("Test1", "", false))
-                println("Button1 run")
-                println(testTaskList)
-            }) {
-                Text("Option 1")
-            }
-
-            Button(onClick = { navController.navigate("taskScreen") {
-                popUpTo("taskScreen"){
-                    inclusive = true
+            composable("editScreen/{id}"){
+                val id = it.arguments!!.getString("id")!!.toInt()
+                if(id < testTaskList.size){
+                    EditScreen(testTaskList[id], navController)
                 }
-            } }) {
-                Text("Option 2")
             }
+        }
+        ButtonBar(navController = navController)
+    }
+}
 
-            Button(onClick = { navController.navigate("createScreen"){
-                launchSingleTop = true
-                popUpTo("taskScreen")
-            } }) {
-                Text("Option 3")
+@Composable
+fun ButtonBar(navController: NavHostController){
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Yellow)
+    ){
+
+        Button(onClick = {
+            testTaskList.add(TaskItem("Test1", testTaskList.size))
+            println("Button1 run")
+            println(testTaskList)
+        }) {
+            Text("Add Dummy")
+        }
+
+        Button(onClick = { navController.navigate("taskScreen") {
+            popUpTo("taskScreen"){
+                inclusive = true
             }
+        } }) {
+            Text("Home")
+        }
+
+        Button(onClick = { navController.navigate("createScreen"){
+            launchSingleTop = true
+            popUpTo("taskScreen")
+        } }) {
+            Text("Add Task")
         }
     }
 }
 
 @Composable
-fun TaskScreen(tasks: MutableList<TaskItem>) {
+fun TaskScreen(navController: NavHostController) {
+    var tasks = testTaskList
+    println("Loading Task Screen")
     LazyColumn(
-        modifier = Modifier.background(Color.White).fillMaxWidth().padding(8.dp)
+        modifier = Modifier
+            .fillMaxWidth()
     ){
         if(tasks.isEmpty()){
             item{
@@ -113,9 +140,29 @@ fun TaskScreen(tasks: MutableList<TaskItem>) {
             }
         }
         items(tasks){task ->
-            Text(task.taskTitle,
+            println("Printing Task : $task")
+            Card(
+                shape = RoundedCornerShape(5.dp),
+                border = BorderStroke(2.dp, Color.Gray),
+                elevation = 10.dp,
                 modifier = Modifier
-            )
+                    .fillMaxWidth()
+                    .padding(4.dp)
+                    .clickable{
+                        navController.navigate("editScreen/${task.taskId}")
+                    }
+            ){
+                Row(
+                    horizontalArrangement = Arrangement.SpaceAround
+                ){
+                    Text(task.taskTitle,
+                        modifier = Modifier
+                            .padding(4.dp)
+                    )
+                }
+
+            }
+
         }
     }
 }
@@ -123,29 +170,89 @@ fun TaskScreen(tasks: MutableList<TaskItem>) {
 @Composable
 fun CreateScreen() {
     var textFieldValue by remember { mutableStateOf(TextFieldValue(""))}
+    //val focusManager = LocalFocusManager.current
     Column(){
-        OutlinedTextField(value = textFieldValue, onValueChange = {
-            textFieldValue = it
-        },
-            label = {Text(text = "Task Title")},
-            placeholder = {Text(text = "eg. Wash dishes..")},
-            trailingIcon = {Icon(
-                Icons.Filled.Add,
-                contentDescription = "Add task",
-                modifier = Modifier.clickable {
-                    //textFieldValue.text = ""
-                    testTaskList.add(TaskItem(taskTitle = textFieldValue.text, "", false))
-                }
-            )}
-        )
-        /*Button(onClick = {
-            testTaskList.add(TaskItem(taskTitle = textFieldValue.text, "", false))
-
-        }) {
-            Text("Add")
-        }*/
+        Column(
+            modifier = Modifier
+        ){
+            OutlinedTextField(value = textFieldValue, modifier = Modifier.background(Color.White), onValueChange = {
+                textFieldValue = it
+            },
+                label = {Text(text = "Task Title")},
+                placeholder = {Text(text = "eg. Wash dishes..")},
+                trailingIcon = {Icon(
+                    Icons.Filled.Add,
+                    contentDescription = "Add task",
+                    modifier = Modifier
+                        .clickable {
+                            if(textFieldValue.text.isNotEmpty()){
+                                testTaskList.add(TaskItem(taskTitle = textFieldValue.text, testTaskList.size))
+                                textFieldValue = TextFieldValue("")
+                                //focusManager.clearFocus()
+                            }
+                    }
+                )}
+            )
+        }
     }
+}
 
+fun removeItemAndRemakeListWithNewItemIds(task: TaskItem): MutableList<TaskItem> {
+    var newList: MutableList<TaskItem> = mutableListOf()
+    testTaskList.forEach {
+        if (task != it) {
+            var newTask = TaskItem(it.taskTitle, newList.size)
+            newList.add(newTask)
+            println("New Task: $newTask")
+        }
+    }
+    println("Size: ${newList.size} List: $newList")
+    return newList
+}
+
+@Composable
+fun EditScreen(taskSent: TaskItem, navController: NavHostController) {
+    //val task = testTaskList[taskId]
+    var task = taskSent
+    var textFieldValue by remember { mutableStateOf(TextFieldValue(task.taskTitle))}
+    Column(){
+        Column(
+            modifier = Modifier
+        ){
+            OutlinedTextField(value = textFieldValue, modifier = Modifier.background(Color.White), onValueChange = {
+                textFieldValue = it
+            },
+                label = {Text(text = "Task Title")},
+                placeholder = {Text(text = "eg. Wash dishes..")},
+                trailingIcon = {Icon(
+                    Icons.Filled.Done,
+                    contentDescription = "Add task",
+                    modifier = Modifier
+                        .clickable {
+                            if(textFieldValue.text.isNotEmpty()){
+                                task.taskTitle = textFieldValue.text
+                                navController.navigate("taskScreen"){
+                                    popUpTo("taskScreen"){
+                                        inclusive = true
+                                    }
+                            }
+                        }
+                    }
+                )}
+            )
+            Button(onClick = {
+                testTaskList = removeItemAndRemakeListWithNewItemIds(task)
+                task = TaskItem("", 0)
+                navController.navigate("taskScreen"){
+                    popUpTo("taskScreen"){
+                        inclusive = true
+                    }
+                }
+            }) {
+                Text(text = "Remove Task")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
